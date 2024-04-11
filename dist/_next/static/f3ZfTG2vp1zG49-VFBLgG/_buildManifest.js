c18d2d187fccdb0b4fbc152fa6313b9f20845d01
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2024-04-11 14:58:52 +0200

    fixed popup

--- dist/_next/static/f3ZfTG2vp1zG49-VFBLgG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/37-6de33e27896e983b.js","static/chunks/pages/index-d853b4d19ca212ec.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
